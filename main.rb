549fdde64f3b62c28679bd29e8e97e242ddae1de
Krishna <libraryismyparadise@gmail.com>
2023-03-26 13:17:09 +0545

    Add BMI classification

---
+++ main.rb
@@ -0,0 +1,12 @@
+puts "Body Madd Index(BMI) Calculator"
+puts ( "BMI categories")
+puts ("severe Thinness/anneroxic <16")
+puts ("Moderate Thinness 16-17")
+puts ("Mild Thinness 17-18.5")
+puts ("Normal 18.5-25")
+puts ("Overweight 25-30")
+puts("Obese Class I 30-35")
+puts ("Obese Class II 35-40")
+puts ("Obese Class III >40")
+
+
