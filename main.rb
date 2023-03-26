c2dd72536c4541c44e677cf81a8c52d0e8c9122f
Krishna <libraryismyparadise@gmail.com>
2023-03-26 13:53:48 +0545

    Refactor using case statement instead of if statement

--- main.rb
+++ main.rb
@@ -1,13 +1,4 @@
 puts "Body Madd Index(BMI) Calculator"
-puts  "BMI categories"
-puts "severe Thinness/anneroxic <16"
-puts "Moderate Thinness 16-17"
-puts  "Mild Thinness 17-18.5"
-puts "Normal 18.5-25"
-puts "Overweight 25-30"
-puts "Obese Class I 30-35"
-puts "Obese Class II 35-40"
-puts "Obese Class III >40"
 
 print ( "Height(cm): ")
 #get input convert it into float and assign it into height variable.
@@ -16,6 +7,25 @@ print ("Weight(Kilogram): ")
 #get input convert it into float and assign it into weight variable.
 weight = gets.chomp.to_f
 
-bmi = weight / (height/100)**2
+bmi = (weight / (height/100)**2).floor(2)
 
-puts "BMI  = #{bmi}"
+case bmi
+when 0...16
+  puts "Your BMI is #{bmi}. You are suffering from severe Thinness or anorexia. Please consult a doctor as soon as possible."
+when 16...17
+  puts "Your BMI is #{bmi}. You are suffering from Moderate Thinness. Please consult a doctor."
+when 17...18.5
+  puts "Your BMI is #{bmi}. You have Mild Thinness. Please consult a Dietician."
+when 18.5...25
+  puts "Your BMI is #{bmi}. You are perfectly normal. Keep up the good work!"
+when 25...30
+  puts "Your BMI is #{bmi}. You are a little bit overweight. Watch out your calories."
+when 30...35
+  puts "Your BMI is #{bmi}. You have Obesity Class I. Watch out your total calorie, it might be a good idea to hit the gym."
+when 35...40
+  puts "Your BMI is #{bmi}. You have Obesity Class II. You should consider consulting a doctor and a dietician."
+else
+  puts "Your BMI is #{bmi}. You have Obesity Class III. Please consult a doctor as soon as possible."
+end
+
+
